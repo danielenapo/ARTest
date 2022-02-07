23e8c577fbef915352b44f4c6520952d248278ad
danielenapo <gnele2000@gmail.com>
2022-02-07 21:35:15 +0100

    import reference images from gallery

--- Assets/Scripts/DynamicLibrary.cs
+++ Assets/Scripts/DynamicLibrary.cs
@@ -14,6 +14,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
     [RequireComponent(typeof(ARTrackedImageManager))]
     public class DynamicLibrary : MonoBehaviour
     {
+        //CLASSE PER GESTIONE DATI TEXTURE
         [Serializable]
         public class ImageData
         {
@@ -48,7 +49,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
         }
 
         [SerializeField, Tooltip("The set of images to add to the image library at runtime")]
-        ImageData[] m_Images;
+        ImageData[] m_Images; //vettore dei dati
 
         /// <summary>
         /// The set of images to add to the image library at runtime
@@ -87,35 +88,37 @@ namespace UnityEngine.XR.ARFoundation.Samples
             switch (m_State)
             {
                 case State.NoImagesAdded:
-                {
-                    if (GUILayout.Button("Add images"))
                     {
-                        m_State = State.AddImagesRequested;
-                    }
+                        if (GUILayout.Button("Add images"))
+                        {
 
-                    break;
-                }
-                case State.AddingImages:
-                {
-                    m_StringBuilder.Clear();
-                    m_StringBuilder.AppendLine("Add image status:");
-                    foreach (var image in m_Images)
-                    {
-                        m_StringBuilder.AppendLine($"\t{image.name}: {(image.jobState.status.ToString())}");
+                            PickImage();
+
+                        }
+
+                        break;
                     }
-                    GUILayout.Label(m_StringBuilder.ToString());
-                    break;
-                }
+                case State.AddingImages:
+                    {
+                        m_StringBuilder.Clear();
+                        m_StringBuilder.AppendLine("Add image status:");
+                        foreach (var image in m_Images)
+                        {
+                            m_StringBuilder.AppendLine($"\t{image.name}: {(image.jobState.status.ToString())}");
+                        }
+                        GUILayout.Label(m_StringBuilder.ToString());
+                        break;
+                    }
                 case State.Done:
-                {
-                    GUILayout.Label("All images added");
-                    break;
-                }
+                    {
+                        GUILayout.Label("All images added");
+                        break;
+                    }
                 case State.Error:
-                {
-                    GUILayout.Label(m_ErrorMessage);
-                    break;
-                }
+                    {
+                        GUILayout.Label(m_ErrorMessage);
+                        break;
+                    }
             }
 
             GUILayout.EndArea();
@@ -132,79 +135,123 @@ namespace UnityEngine.XR.ARFoundation.Samples
             switch (m_State)
             {
                 case State.AddImagesRequested:
-                {
-                    if (m_Images == null)
                     {
-                        SetError("No images to add.");
-                        break;
-                    }
-
-                    var manager = GetComponent<ARTrackedImageManager>();
-                    if (manager == null)
-                    {
-                        SetError($"No {nameof(ARTrackedImageManager)} available.");
-                        break;
-                    }
-
-                    // You can either add raw image bytes or use the extension method (used below) which accepts
-                    // a texture. To use a texture, however, its import settings must have enabled read/write
-                    // access to the texture.
-                    foreach (var image in m_Images)
-                    {
-                        if (!image.texture.isReadable)
+                        if (m_Images == null)
                         {
-                            SetError($"Image {image.name} must be readable to be added to the image library.");
+                            SetError("No images to add.");
                             break;
                         }
-                    }
 
-                    if (manager.referenceLibrary is MutableRuntimeReferenceImageLibrary mutableLibrary)
-                    {
-                        try
+                        var manager = GetComponent<ARTrackedImageManager>();
+                        if (manager == null)
                         {
-                            foreach (var image in m_Images)
+                            SetError($"No {nameof(ARTrackedImageManager)} available.");
+                            break;
+                        }
+
+                        // You can either add raw image bytes or use the extension method (used below) which accepts
+                        // a texture. To use a texture, however, its import settings must have enabled read/write
+                        // access to the texture.
+                        foreach (var image in m_Images)
+                        {
+                            if (!image.texture.isReadable)
                             {
-                                // Note: You do not need to do anything with the returned JobHandle, but it can be
-                                // useful if you want to know when the image has been added to the library since it may
-                                // take several frames.
-                                image.jobState = mutableLibrary.ScheduleAddImageWithValidationJob(image.texture, image.name, image.width);
+                                SetError($"Image {image.name} must be readable to be added to the image library.");
+                                break;
                             }
+                        }
 
-                            m_State = State.AddingImages;
-                        }
-                        catch (InvalidOperationException e)
+                        if (manager.referenceLibrary is MutableRuntimeReferenceImageLibrary mutableLibrary)
                         {
-                            SetError($"ScheduleAddImageJob threw exception: {e.Message}");
+                            try
+                            {
+                                foreach (var image in m_Images)
+                                {
+                                    // Note: You do not need to do anything with the returned JobHandle, but it can be
+                                    // useful if you want to know when the image has been added to the library since it may
+                                    // take several frames.
+                                    image.jobState = mutableLibrary.ScheduleAddImageWithValidationJob(image.texture, image.name, image.width);
+                                }
+
+                                m_State = State.AddingImages;
+                            }
+                            catch (InvalidOperationException e)
+                            {
+                                SetError($"ScheduleAddImageJob threw exception: {e.Message}");
+                            }
                         }
+                        else
+                        {
+                            SetError($"The reference image library is not mutable.");
+                        }
+
+                        break;
+                    }
+                case State.AddingImages:
+                    {
+                        // Check for completion
+                        var done = true;
+                        foreach (var image in m_Images)
+                        {
+                            if (!image.jobState.jobHandle.IsCompleted)
+                            {
+                                done = false;
+                                break;
+                            }
+                        }
+
+                        if (done)
+                        {
+                            m_State = State.Done;
+                        }
+
+                        break;
+                    }
+            }
+        }
+        private void PickImage()
+        {
+            NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+            {
+                Debug.Log("Image path: " + path);
+                if (path != null)
+                {
+                    // Create Texture from selected image
+                    Texture2D texture = NativeGallery.LoadImageAtPath(path);
+                    if (texture != null)
+                    {
+                        texture = AdaptTexture(texture);
+                        m_Images[0].texture = texture;
+                        m_State = State.AddImagesRequested;
                     }
                     else
                     {
-                        SetError($"The reference image library is not mutable.");
+                        Debug.Log("Couldn't load texture from " + path);
+                        return;
                     }
 
-                    break;
                 }
-                case State.AddingImages:
-                {
-                    // Check for completion
-                    var done = true;
-                    foreach (var image in m_Images)
-                    {
-                        if (!image.jobState.jobHandle.IsCompleted)
-                        {
-                            done = false;
-                            break;
-                        }
-                    }
+            }, "", "image/*");
+        }
 
-                    if (done)
-                    {
-                        m_State = State.Done;
-                    }
+        private Texture2D AdaptTexture(Texture2D source)
+        {
+            RenderTexture renderTex = RenderTexture.GetTemporary(
+                        source.width,
+                        source.height,
+                        0,
+                        RenderTextureFormat.Default,
+                        RenderTextureReadWrite.Linear);
 
-                    break;
-                }
-            }
+            Graphics.Blit(source, renderTex);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = renderTex;
+            Texture2D readableText = new Texture2D(source.width, source.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTex);
+            return readableText;
         }
     }
 }
